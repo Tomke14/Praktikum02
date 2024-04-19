64b8836ca80cb49ed9429446563d65d5cad33784
Tomke14 <165892139+Tomke14@users.noreply.github.com>
2024-04-19 11:17:56 +0200

    add Main class

--- Streichholzspiel/src/Eingabe.java
+++ Streichholzspiel/src/Eingabe.java
@@ -1,8 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Eingabe {
 
     public static int leseZahl(){
         Ausgabe.zahlEingeben();
-        return 0;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int zahl = Integer.parseInt(reader.readLine());
+            return zahl;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+
+        catch (NumberFormatException e) {
+            Ausgabe.keineZahl();
+            return leseZahl();
+        }
+    }
+
+    public static int leseHoelzer(){
+        int zahl = leseZahl();
+        if (zahl < 1 || zahl > 3) {
+            Ausgabe.zahlNichtImBereich();
+            return leseHoelzer();
+        } else {
+            return zahl;
+        }
     }
 
 }
---
+++ Streichholzspiel/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Spiel spiel = new Spiel(18);
+    }
+}
